4a6d076c755d05a41a377a0a9d25557aa9f6ffd4
mangert <mangert@yandex.ru>
2025-03-21 16:29:20 +0300

    frontend intm commit - manualy added types, started page.tx

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,103 +1,78 @@
-import Image from "next/image";
+"use client";
+
+import React, {
+  useState,
+  useEffect, 
+}  from "react";
+import { ethers } from "ethers";
+
+import { MagicCard__factory } from "@/typechain";
+import type { MagicCard } from "@/typechain";
+import type { BrowserProvider } from "ethers";
+
+import ConnectWallet from "@/components/ConnectWallet";
+import WaitinForTransactionMessage from "@/components/WaitingForTransactionMessage";
+import TransactionErrorMessage from "@/components/TransactionErrorMessage";
+
+const HARDHAT_NETWORK_ID = "0x539";
+const MAGIC_CARD_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+declare let window: any;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const [networkError, setNetworkError] = useState<string>();
+  
+    const _connectWallet = async() => {
+        if(window.ethereum === undefined){
+          setNetworkError ("Please install Metamask");
+          return;
+        }
+        if (!(await _checkNetwork())) {
+          return; 
+      }
+  
+      const [selectedAccount] = await window.ethereum.request({
+        method: "eth_requestAccounts"
+      });
+  
+      await _initialize(ethers.getAddress(selectedAccount));
+  
+      window.ethereum.on("accountsChanged", 
+        async ([newAccount] : [newAccount: string]) => {
+          if(newAccount === undefined){
+            return _resetState();
+          }
+          await _initialize(ethers.getAddress(selectedAccount));
+        }
+      );
+  
+      window.ethereum.on("chainChanged", 
+        async ([_networkId] : any) => {
+          _resetState;      
+        }
+      );
+    }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const _initialize = async(selectedAccount: string) => {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner(selectedAccount);
+    }
+
+
+    const _checkNetwork() => async (): Promise<boolean> => {
+      const chosenChainId = await window.ethereum.request({
+        method: "eth_chainId",
+      });
+      if (chosenChainId === HARDHAT_NETWORK_ID){
+        return true;
+      }
+      setNetworkError("Please connect to HardHat network (localhost:8545)");
+      return false;
+    };
+
+    return (
+      <main>
+      
+      </main>   
+    );
 }
