e6a9fdbdc440c35572549772f3d266b5eb039ba7
mangert <mangert@yandex.ru>
2025-03-25 13:32:38 +0300

    config settings for sepolia deploy added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config(); // Загружаем .env
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-  networks : {
-    hardhat : {
-      chainId : 1337,
-      initialBaseFeePerGas : 0,
-    },   
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      initialBaseFeePerGas: 0,
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL || "", // Берем URL из .env
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+      chainId: 11155111, // Sepolia chainId
+    },
   },
 };
 
 export default config;
+
