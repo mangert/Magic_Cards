4c8139abd43fcf49bb9a63ceada11a4a030598b6
mangert <mangert@yandex.ru>
2025-04-06 00:23:20 +0300

    test for descCreate function added

--- test/setup.ts
+++ test/setup.ts
@@ -2,5 +2,6 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import "@nomicfoundation/hardhat-chai-matchers";
+import { MagicCard } from './typechain-types/contracts/MagicCard.ts';
 
 export { loadFixture, ethers, expect };
